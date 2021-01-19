3c4ce7821660be3572e3082d540c662bfb701655
jagtrrz <jagutierrezc7@gmail.com>
2021-01-19 19:16:31 +0000

    trying to know how js/dom works by eventscode

--- src/main.js
+++ src/main.js
@@ -5,6 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let myButton = document.querySelector("#mySend");
+  let creditCard = document.querySelector("#myCard");
+
+  //  myButton.addEventListener("click", event => {
+  //    console.log(event);
+  //    if (event.size == 16) {
+  //      event.preventDefault();
+  //    }
+
+  creditCard.addEventListener("keypress", event => {
+    console.log(event);
+    if (23) {
+      console.log("estas pulsadno numeros");
+    } else {
+      console.log("no estas pulsando numeros");
+      creditCard.classList.add("bg-danger");
+    }
+  });
+
+  creditCard.addEventListener("change", event => {
+    console.log(event);
+  });
+
+  // });
 };
