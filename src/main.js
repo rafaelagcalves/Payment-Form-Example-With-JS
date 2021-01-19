6e0c5ddad0be3f4f9122fd02fc53c6a0748780b5
jagtrrz <jagutierrezc7@gmail.com>
2021-01-19 20:23:27 +0000

    trying a new conditional for text input

--- src/main.js
+++ src/main.js
@@ -6,8 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  let myButton = document.querySelector("#mySend");
+  //let form = document.querySelectorAll("#form");
+  //form.addEventListener("submit", validateForm());
+
+  //function validateForm(evento) {
+
+  //evento.preventDefault();
+
   let creditCard = document.querySelector("#myCard");
+  creditCard.addEventListener("input", event => {
+    console.log(event);
+    if (event.data >= 0) {
+      console.log("has tecleado numeros");
+    } else {
+      console.log("has tecleado algo que no es un numero");
+      creditCard.classList.add("bg-danger");
+    }
+  });
+
+  //this.submit();
+  //}
+
+  //let myButton = document.querySelector("#mySend");
+  //let creditCard = document.querySelector("#myCard");
 
   //  myButton.addEventListener("click", event => {
   //    console.log(event);
@@ -15,19 +36,19 @@ window.onload = () => {
   //      event.preventDefault();
   //    }
 
-  creditCard.addEventListener("keypress", event => {
-    console.log(event);
-    if (23) {
-      console.log("estas pulsadno numeros");
-    } else {
-      console.log("no estas pulsando numeros");
-      creditCard.classList.add("bg-danger");
-    }
-  });
+  //creditCard.addEventListener("keypress", event => {
+  //console.log(event);
+  //     if (23) {
+  //       console.log("estas pulsadno numeros");
+  //     } else {
+  //       console.log("no estas pulsando numeros");
+  //       creditCard.classList.add("bg-danger");
+  //     }
+  //   });
 
-  creditCard.addEventListener("change", event => {
-    console.log(event);
-  });
+  //   creditCard.addEventListener("change", event => {
+  //     console.log(event);
+  //   });
 
   // });
 };
